69c7e0b2c8e975625e51e521178a7fd9873b3105
alahmnat <alahmnat@mac.com>
2017-01-02 14:28:28 -0800

    Added project files
    Added all of the project files, and updated the readme with
    documentation on how to use the DniDate() object.

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # dni-date-converter
+
+The D'ni Date Converter is a JavaScript implementation of the D'ni↔Gregorian calendar conversion algorithms originally developed by Brett Middleton. Details about the algorithms themselves can be found [here](https://archive.guildofarchivists.org/wiki/D%27ni_time_conversion).
+
+This project contains both the converter and a website which can be used to convert arbitrary dates between the two calendar systems. To use the date converter in your own project, you should only need the DniDate object's declaration, which is in `js/DniDate.js`. 
+
+## DniDate() Object
+
+This object is modeled on the native JavaScript `Date()` object, and supports manipulation of dates in the D'ni calendar, just as the `Date()` object supports manipulating dates in the Gregorian calendar.
+
+### Creating a new date
+
+You can create a new `DniDate()` in one of three ways.
+
+#### Supply a D'ni date
+
+ `var dd = new DniDate(hahr, vailee, yahr, gartahvo, tahvo, gorahn, prorahn);`
+
+#### Call `DniDate.now()`
+
+`var dd = DniDate.now();`
+
+#### Create from Gregorian date
+
+Generate a new `DniDate()`, then pass a `Date()` object into the `setFromSurfaceDate()` method: 
+
+`var dd = new DniDate(); `
+`dd.setFromSurfaceDate(surfaceDate);`
+
+**NOTE:** If you create a Gregorian date between 1 CE and 99 CE (1 - 99) by calling `new Date(year, month, day)`, the `Date()` object will "helpfully" set your date in the 20th century (1901 - 1999). To avoid this, create the `Date()` object, then call `setFullYear()` with your desired year before passing it into `DniDate()`.
+
+### Converting dates
+
+#### "Surface" (Gregorian) date to D'ni
+
+Follow the instructions above, under "Create from Gregorian date".
+
+#### D'ni date to Gregorian date
+
+Create a `DniDate()` object with the desired D'ni date, then call `toSurfaceDate()`. This will return a new `Date()` object set to the equivalent date in the Gregorian calendar.
+
+### Modifying the date
+
+You can use one of the `setX` methods to alter a particular component of the D'ni date once it has been created. There are set methods for the following date components:
+
+* Hahr
+* Vailee (0-indexed)
+* Yahr
+* Gartahvo
+* Tahvo
+* Gorahn
+* Prorahn
+
+If you supply a value for a date component that is out of range (such as calling `setVailee(10)` when the valid range is 0 through 9), a valid date will automatically be calculated. In this case, `hahr` will be incremented by 1, and `vailee` will be set to 0.
+
+You many notice that there is a `getPartahvo()`, method, but no `setPartahvo()` method. This is because `DniDate()` treats the partahvo as a calculated unit, rather than a specified one. The value of the partahvo in a given date is determined by dividing the number of tahvotee by 5, then rounding down.
+
+### Retrieving / formatting D'ni dates
+
+There are three built-in methods for displaying a D'ni date:
+
+1. `toDateString()`: Returns the vailee, yahr, and hahr (month, day, year), as well as the era (BE or DE).
+2. `toTimeString()`: Returns the gartahvo, tahvo, gorahn, and prorahn, separated by colons. The tahvo, gorahn, and prorahn values are zero-padded to two digits in this output for better legibility.
+3. `toString()`: Returns both of the above values, concatenated together by a space.
+
+Alternatively, you can build your own date format using the following getters:
+
+* `getHahr()`
+* `getVailee()` (returns a value between 0 and 9 representing the numerical vailee value)
+* `getVaileeName()` (returns the actual vailee name, written in [OTS](https://archive.guildofarchivists.org/wiki/D%27ni_(language)#Old_Transliteration_Standard)).
+* `getYahr()`
+* `getGartahvo()`
+* `getPartahvo()` (not commonly used; this value divides the D'ni yahr into 5 segments, rather than the tahvo's 25)
+* `getTahvo()`
+* `getGorahn()`
+* `getProrahn()`
---
+++ js/DniDate.js
@@ -0,0 +1,299 @@
+/*!
+ * D'niDate 1.0
+ * Copyright 2016 Gary Buddell
+ * Formulas provided by Brett Middleton: https://archive.guildofarchivists.org/wiki/D'ni_time_conversion
+ * Based on the D'ni time system developed by Richard Watson and Cyan, Inc.
+ * Licensed under the MIT license
+ */
+function DniDate(hahr, vailee, yahr, gartahvo, tahvo, gorahn, prorahn) {
+    var hahr = hahr;
+    var vailee = vailee;
+    var yahr = yahr;
+    var gartahvo = gartahvo;
+    var tahvo = tahvo;
+    var gorahn = gorahn;
+    var prorahn = prorahn;
+
+    function adjust() {
+        while (this.prorahn > 25) {
+            this.prorahn = this.prorahn - 25;
+            this.gorahn = this.gorahn + 1;
+        }
+        while (this.prorahn < 0) {
+            this.prorahn = this.prorahn + 25;
+            this.gorahn = this.gorahn - 1;
+        }
+
+        while (this.gorahn > 25) {
+            this.gorahn = this.gorahn - 25;
+            this.tahvo = this.tahvo + 1;
+        }
+        while (this.gorahn < 0) {
+            this.gorahn = this.gorahn + 25;
+            this.tahvo = this.tahvo - 1;
+        }
+
+        while (this.tahvo > 25) {
+            this.tahvo = this.tahvo - 25;
+            this.gartahvo = this.gartahvo + 1;
+        }
+        while (this.tahvo < 0) {
+            this.tahvo = this.tahvo + 25;
+            this.gartahvo = this.gartahvo - 1;
+        }
+
+        while (this.gartahvo > 5) {
+            this.gartahvo = this.gartahvo - 25;
+            this.yahr = this.yahr + 1;
+        }
+        while (this.gartahvo < 0) {
+            this.gartahvo = this.gartahvo + 5;
+            this.yahr = this.yahr - 1;
+        }
+
+        while (this.yahr > 29) {
+            this.yahr = this.yahr - 29;
+            this.vailee = this.vailee + 1;
+        }
+        while (this.yahr < 0) {
+            this.yahr = this.yahr + 29;
+            this.vailee = this.vailee - 1;
+        }
+
+        while (this.vailee > 9) {
+            this.vailee = this.vailee - 10;
+            this.hahr = this.hahr + 1;
+        }
+        while (this.vailee < 0) {
+            this.vailee = this.vailee + 10;
+            this.hahr = this.hahr - 1;
+        }
+    }
+
+    function julianDayNumberToGregorianDate(jdn) {
+        var z = Math.floor(jdn);
+        var g = z - 0.25;
+        var a = Math.floor(g / 36524.25);
+        var b = a - (0.25 * a);
+        var year = Math.floor((g + b) / 365.25);
+        var c = z + b - Math.floor(365.25 * year);
+        var month = ~~(((5 * c) + 456) / 153);
+        var day = Math.ceil(c - ~~(((153 * month) - 457) / 5));
+
+        if (month > 12) {
+            year = year + 1;
+            month = month - 12;
+        }
+
+        if (year < 1) {
+            year = year - 1;
+        }
+
+        z = (jdn - Math.floor(jdn)) * 86400;
+        var hour = ~~(z / 3600);
+        var r = z - (hour * 3600);
+        var minute = ~~(r / 60);
+        var second = Math.ceil(r - (minute * 60));
+
+        var surfaceDate = new Date(Date.UTC(year, month - 1, day, hour, minute, second));
+        surfaceDate.setFullYear(year);
+        
+        return surfaceDate;
+    }
+
+    function gregorianDateToJulianDayNumber(gDate) {
+        var year = gDate.getFullYear();
+        var month = gDate.getMonth() + 1; // The DniDate output's month may be 0-indexed, but the math isn't
+        var day = gDate.getDate();
+        var hour = gDate.getHours();
+        var minute = gDate.getMinutes();
+        var second = gDate.getSeconds();
+
+        if (month < 3) {
+            month = month + 12;
+            year = year - 1;
+        }
+
+        var wholeDays = day + parseFloat(~~(((153 * month) - 457) / 5)) + parseFloat(Math.floor(365.25 * year)) - parseFloat(Math.floor(0.01 * year)) + parseFloat(Math.floor(0.0025 * year));
+
+        var partialDays = ((hour * 3600) + (minute * 60) + second) / 86400;
+
+        return wholeDays + partialDays;
+    }
+
+    function setAtrianDayNumberToCavernDate(adn) {
+        var z = parseFloat(Math.floor(adn));
+        var g = z - 0.25;
+        var a = parseFloat(Math.floor(g / 290));
+        hahr = 9647 + a;
+        var c = z - (a * 290);
+        vailee = parseFloat(Math.floor((c - 0.25) / 29)) + 1;
+        yahr = c - ((vailee - 1) * 29);
+
+        z = (adn - Math.floor(adn)) * 78125;
+        gartahvo = ~~(z / 15625);
+        var r = z - (gartahvo * 15625);
+        tahvo = ~~(r / 625);
+        r = r - (tahvo * 625);
+        gorahn = ~~(r / 25);
+        prorahn = Math.floor(r - (gorahn * 25));
+        
+        vailee = vailee - 1;
+    }
+
+    function cavernDateToAtrianDayNumber() {
+        var wholeDays = parseInt(yahr) + ((vailee - 1) * 29) + ((hahr - 9647) * 290);
+        
+        var partialDays = ((gartahvo * 15625) + (tahvo * 625) + (gorahn * 25) + prorahn) / 78125;
+
+        return wholeDays + partialDays;
+    }
+
+    this.getHahr = function () {
+        return hahr;
+    }
+    this.setHahr = function (h) {
+        hahr = h;
+        adjust();
+    }
+
+    this.getVailee = function () {
+        return vailee;
+    }
+    this.setVailee = function (v) {
+        vailee = v;
+        adjust();
+    }
+
+    this.getYahr = function () {
+        return yahr;
+    }
+    this.setYahr = function (y) {
+        yahr = y;
+        adjust();
+    }
+
+    this.getGartahvo = function () {
+        return gartahvo;
+    }
+    this.setGartahvo = function (g) {
+        gartahvo = g;
+        adjust();
+    }
+
+    this.getPartahvo = function () {
+        return Math.floor(tahvo / 5);
+    }
+
+    this.getTahvo = function () {
+        return tahvo;
+    }
+    this.setTahvo = function (t) {
+        tahvo = t;
+        adjust();
+    }
+
+    this.getGorahn = function () {
+        return gorahn;
+    }
+    this.setGorahn = function (g) {
+        gorahn = g;
+        adjust();
+    }
+
+    this.getProrahn = function () {
+        return prorahn;
+    }
+    this.setProrahn = function (p) {
+        prorahn = p;
+        adjust();
+    }
+
+    this.toString = function () {
+        return this.toDateString() + " " + this.toTimeString();
+    }
+
+    this.toDateString = function () {
+        if (hahr < 0) {
+            return this.getVaileeName() + " " + yahr + " " + (hahr * -1) + " BE";
+        }
+        else {
+            return this.getVaileeName() + " " + yahr + " " + hahr + " DE";
+        }
+    }
+
+    this.toTimeString = function () {
+        return gartahvo + ":" + tahvo.pad(2) + ":" + gorahn.pad(2) + ":" + prorahn.pad(2);
+    }
+
+    this.getVaileeName = function () {
+        switch (vailee) {
+            case 0:
+                return "Leefo";
+                break;
+            case 1:
+                return "Leebro";
+                break;
+            case 2:
+                return "Leesahn";
+                break;
+            case 3:
+                return "Leetar";
+                break;
+            case 4:
+                return "Leevot";
+                break;
+            case 5:
+                return "Leevofo";
+                break;
+            case 6:
+                return "Leevobro";
+                break;
+            case 7:
+                return "Leevosahn";
+                break;
+            case 8:
+                return "Leevotar";
+                break;
+            case 9:
+                return "Leenovoo";
+                break;
+        }
+    }
+
+    this.setFromSurfaceDate = function (surface) {
+        var jdn = gregorianDateToJulianDayNumber(surface);
+        var jdd = jdn - 727249.745833333;
+        var add = jdd * 0.793993705929756;
+
+        var adn = add + 1;
+
+        setAtrianDayNumberToCavernDate(adn);
+    }
+
+    this.toSurfaceDate = function () {
+        var adn = cavernDateToAtrianDayNumber();
+        var add = adn - 1;
+
+        var jdd = add * 1.25945582758621;
+        var jdn = jdd + 727249.745833333;
+
+        return julianDayNumberToGregorianDate(jdn);
+    }
+
+    this.now = function () {
+        this.setFromSurfaceDate(new Date());
+    }
+    
+    if(arguments.length == 0) {
+	    this.setFromSurfaceDate(new Date());
+    }
+
+    adjust();
+}
+
+Number.prototype.pad = function(size) {
+      var s = String(this);
+      while (s.length < (size || 1)) {s = "0" + s;}
+      return s;
+    }
